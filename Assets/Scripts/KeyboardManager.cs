1f27f905ea4790e9f935efd503007a06a469561c
her0iin <her0in941018@gmail.com>
2020-11-05 23:12:37 +0900

    User Info Layer updated
    User Info Layer updated

---
+++ Assets/Scripts/KeyboardManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class KeyboardManager : MonoBehaviour
+{
+	InputField inputField;
+	private void Awake()
+	{
+		inputField = GetComponent<InputField>();
+	}
+
+	void Start()
+    {
+		inputField.ActivateInputField();
+		inputField.Select();
+	}
+}
