3244bacc1ba31c0cea5a903d18e92d1a23573f9b
her0iin <her0in941018@gmail.com>
2020-11-13 20:49:01 +0900

    Login Layer Added
    Login Layer Added

--- Assets/Scripts/ShowApplyedTeamInfo.cs
+++ Assets/Scripts/ShowApplyedTeamInfo.cs
@@ -1,11 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShowApplyedTeamInfo : MonoBehaviour
 {
+	public Text title;
+	public Text position;
+	public Text tool;
+
+	private void OnEnable()
+	{
+		SetPanelInfo();
+	}
+
 	public void SetInfo()
 	{
 		UserManager.Instance.SetApplyedTeamInfo(TeamInfoDisplayer.Instance.teamInfo);
+	}
+
+	void SetPanelInfo()
+	{
+		if (!TeamInfoDisplayer.Instance.teamInfo) return;
+
+		title.text = TeamInfoDisplayer.Instance.teamInfo.title;
+		position.text = TeamInfoDisplayer.Instance.teamInfo.positions;
+		tool.text = TeamInfoDisplayer.Instance.teamInfo.toolKR;
 	}
 }
